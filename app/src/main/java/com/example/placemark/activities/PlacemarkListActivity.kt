56b776ab497862fad92b47849c5c423566921faa
Berncat <b.cattigan1@hotmail.com>
2022-10-17 14:39:30 +0100

    Fourth Commit

--- app/src/main/java/com/example/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/com/example/placemark/activities/PlacemarkListActivity.kt
@@ -8,10 +8,12 @@ import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.placemark.R
 import com.example.placemark.adapters.PlacemarkAdapter
+import com.example.placemark.adapters.PlacemarkListener
 import com.example.placemark.databinding.ActivityPlacemarkListBinding
 import com.example.placemark.main.MainApp
+import com.example.placemark.models.PlacemarkModel
 
-class PlacemarkListActivity : AppCompatActivity() {
+class PlacemarkListActivity : AppCompatActivity(), PlacemarkListener {
 
     lateinit var app: MainApp
     private lateinit var binding: ActivityPlacemarkListBinding
@@ -27,7 +29,8 @@ class PlacemarkListActivity : AppCompatActivity() {
 
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
-        binding.recyclerView.adapter = PlacemarkAdapter(app.placemarks)
+        // binding.recyclerView.adapter = PlacemarkAdapter(app.placemarks)
+        binding.recyclerView.adapter = PlacemarkAdapter(app.placemarks.findAll(),this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -43,5 +46,16 @@ class PlacemarkListActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPlacemarkClick(placemark: PlacemarkModel) {
+        val launcherIntent = Intent(this, PlacemarkActivity::class.java)
+        launcherIntent.putExtra("placemark_edit", placemark)
+        startActivityForResult(launcherIntent,0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
--- app/src/main/java/com/example/placemark/main/MainApp.kt
+++ app/src/main/java/com/example/placemark/main/MainApp.kt
@@ -1,13 +1,15 @@
 package com.example.placemark.main
 
 import android.app.Application
-import com.example.placemark.models.PlacemarkModel
+import com.example.placemark.models.PlacemarkMemStore
+// import com.example.placemark.models.PlacemarkModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp : Application() {
 
-    val placemarks = ArrayList<PlacemarkModel>()
+    // val placemarks = ArrayList<PlacemarkModel>()
+    val placemarks = PlacemarkMemStore()
 
     override fun onCreate() {
         super.onCreate()
